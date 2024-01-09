a9996036e6ee4528a3e31a9789a2f6b7fa6bc965
charitywanjiku <charity.wanjiku1@student.moringaschool.com>
2024-01-09 17:29:19 +0300

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+  
+document.addEventListener('DOMContentLoaded', () => {
+  const form = document.getElementById('create-task-form');
+  const taskInput = document.getElementById('new-task-description');
+  const tasksList = document.getElementById('tasks');
+
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const taskValue = taskInput.value.trim();
+    if(taskValue !== ''){
+      
+      const listItem = document.createElement('li');
+
+      
+      const taskText = document.createElement('p');
+      taskText.textContent = taskValue;
+
+    
+      const deleteBtn = document.createElement('button');
+      deleteBtn.textContent = 'Delete';
+      deleteBtn.addEventListener('click', () => {
+        listItem.remove();
+      });
+      
+      
+      listItem.appendChild(taskText);
+      listItem.appendChild(deleteBtn);
+      
+      tasksList.appendChild(listItem);
+      
+      taskInput.value = '';
+    };
+  });
 });
